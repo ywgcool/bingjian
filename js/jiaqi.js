5ba4bdf340707111b39ec631dbf1db417e5ce618
lijiaQ <2433272864@qq.com>
2020-11-27 10:52:45 +0800

    冰鉴

---
+++ js/jiaqi.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+  anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+  sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'tomato', '#000'],
+  showActiveTooltip: true,
+  slidesNavigation: true,
+  navigation: true,
+  menu: '#my-nav'
+})
